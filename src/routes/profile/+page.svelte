790d486a2a3936bd06874de8eb6f1a0b12f422b5
ariel klevecz <ariel@yaytso.art>
2024-12-28 16:37:47 -0800

    update

--- src/routes/profile/+page.svelte
+++ src/routes/profile/+page.svelte
@@ -4,16 +4,25 @@
 
 	/** @type {{ data: import('./$types').PageData }} */
 	let { data } = $props();
+	console.log(authSvelte.state.user)
 </script>
 
 <h1 style="text-align: center;">Profile</h1>
-<div>
-	<div style="text-align: center;">To be implemented...</div>
+<div class="container">
 	{#if authSvelte.state.authorized}
 		<div>Phone: {authSvelte.state.user.phoneNumber}</div>
 		<button onclick={authSvelte.logout}>Logout</button>
 	{/if}
 	{#if !authSvelte.state.authorized}
-		<!-- <AuthFlow /> -->
+		<AuthFlow />
 	{/if}
 </div>
+
+<style>
+	.container {
+		display: flex;
+		align-items: center;
+		flex-direction: column;
+		gap: 1rem;
+	}
+</style>
